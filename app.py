13a83f18bc185282f0be45ec6b38669960587ae3
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-06-05 08:31:38 -0500

    Initial.commit(:yum:)

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request
+from flask_cors import CORS
+from textblob import TextBlob
+from random import choice
+
+app = Flask(__name__)
+CORS(app)
+
+positive_phrases = [
+    "Que bueno, sigue así!",
+    "Me da mucho gusto :D",
+    "Es todo prro!"
+]
+
+negative_phrases = [
+    "Echale ganas mijo",
+    "Ta canijo pero tu puedes",
+    "Animo, todo va a mejorar"
+]
+
+def analyse():
+    polarity = 0
+    subjectivity = 0
+
+    text = request.get_json()['message']
+
+    blob = TextBlob(text)
+    blob = blob.translate(to="en")
+    blob.tags           # [('The', 'DT'), ('titular', 'JJ'),
+                        #  ('threat', 'NN'), ('of', 'IN'), ...]
+
+    blob.noun_phrases   # WordList(['titular threat', 'blob',
+                        #            'ultimate movie monster',
+                        #            'amoeba-like mass', ...])
+
+    for sentence in blob.sentences:
+        polarity = polarity + sentence.sentiment.polarity
+        subjectivity = subjectivity + sentence.sentiment.subjectivity
+
+    return (polarity, subjectivity)
+
+
+@app.route("/", methods=["POST"])
+def index():
+    if request.method == "POST":
+        polarity, subjectivity = analyse()
+
+        if polarity < 0:
+            return ("Tuviste un mal dia basado en tu polaridad de {0}, estoy seguro en un {1}, {2}").format(polarity, subjectivity, choice(negative_phrases))
+        else:
+            return ("Tuviste un buen dia basado en tu polaridad de {0}, estoy seguro en un {1}, {2}").format(polarity, subjectivity, choice(positive_phrases))
+
+
+if __name__ == "__main__":
+    app.run()
